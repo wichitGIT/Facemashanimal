c879010766bde748be959dbae67efeb96cda25fe
wichitGIT <141298280+wichitGIT@users.noreply.github.com>
2024-03-25 21:29:46 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import http from 'http'
 import { app } from './app';
-// const port : number = parseInt(process.env.port || '3000'); //มันจะเอาport เครื่องมา ถ้าไม่ได้ตั้งไว้จะเอาport 3000 มาใช้แทน
+const port : number = parseInt(process.env.port || '3000'); //มันจะเอาport เครื่องมา ถ้าไม่ได้ตั้งไว้จะเอาport 3000 มาใช้แทน
 // const host : string  = process.env.host || '10.160.74.65';
 const server = http.createServer(app);
 
@@ -9,6 +9,6 @@ const server = http.createServer(app);
 // server.listen(port, host, () => {
 //     console.log(`Server is running on http://${host}:${port}`);
 // });
-// server.listen(port,()=>{
-//     console.log("server is started")
-// })
+server.listen(port,()=>{
+    console.log("server is started")
+})
