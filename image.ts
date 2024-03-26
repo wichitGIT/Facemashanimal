169d95b93625565cc8e8bcca18ebab361039e154
wichitGIT <141298280+wichitGIT@users.noreply.github.com>
2024-03-27 00:58:58 +0700

    Update image.ts

--- image.ts
+++ image.ts
@@ -15,7 +15,7 @@ export const conn = mysql.createPool({
 
   
 
-cron.schedule('25 0 * * *', () => {
+cron.schedule('10 0 * * *', () => {
   console.log("hello");
   const queryAsync=util.promisify(conn.query).bind(conn);
 
